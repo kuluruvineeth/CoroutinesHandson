263e7d2d274e5b93c10600201b2fd196393439e5
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-24 17:10:51 +0530

    coroutines sample.

--- app/src/main/java/com/kuluruvineeth/coroutineshandson/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/coroutineshandson/MainActivity.kt
@@ -2,10 +2,22 @@ package com.kuluruvineeth.coroutineshandson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            delay(5000)
+            Log.d(TAG,"Coroutine says hello from thread ${Thread.currentThread().name}")
+        }
+        Log.d(TAG,"hello from thread ${Thread.currentThread().name}")
     }
 }
